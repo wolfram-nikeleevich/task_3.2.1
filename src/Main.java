54a2451e01b6f4cb88aa9b992b0b7002f15c9175
Владимир Сизов <7580844@mail.ru>
2022-02-27 10:46:53 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int ticketPrice = 2020;
         int mileCost = 20;
-        int bonusMiles = ticketPrice/mileCost;
+        int bonusMiles = ticketPrice / mileCost;
         System.out.println("Ваш бонус составляет " + bonusMiles + " миль");
     }
 }
