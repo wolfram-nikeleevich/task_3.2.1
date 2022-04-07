b71331e88266ceed1bce65d0698b9738d3f6e657
Владимир Сизов <7580844@mail.ru>
2022-04-07 14:05:06 +0300

    code reformated

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         int ticketPrice = 2020;
         int mileCost = 20;
         int bonusMiles = ticketPrice / mileCost;
-        System.out.println("Ваш бонус составляет " + bonusMiles + " миль");
+        System.out.println("Ваш бонус составляет " + bonusMiles + " миль ");
     }
 }
